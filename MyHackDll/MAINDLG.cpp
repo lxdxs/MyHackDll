ac9af2e5fd4f477299d7b8f2f00ac0c640d5b8d3
lxdxs <lxdxs@icloud.com>
2019-02-18 11:54:15 +0800

    2.18

--- MyHackDll/MAINDLG.cpp
+++ MyHackDll/MAINDLG.cpp
@@ -9,9 +9,12 @@
 
 
 //游戏Present函数地址
-GD3D11Present oPresent = NULL;
+GD3D11Present oPresent ;
+//游戏DrawIndexed函数地址
+GD3D11DrawIndexed oDrawIndexed ;
+
 //控制台调试信息输出文件句柄
-FILE *g_Stream = NULL;
+FILE *g_Stream ;
 
 
 // CMAINDLG 对话框
@@ -40,6 +43,7 @@ BEGIN_MESSAGE_MAP(CMAINDLG, CDialog)
 	ON_BN_CLICKED(IDC_BUTTON1, &CMAINDLG::OnBnClickedButton1)
 	ON_BN_CLICKED(IDC_CHECK1, &CMAINDLG::OnBnClickedCheck1)
 	ON_BN_CLICKED(IDC_CHECK2, &CMAINDLG::OnBnClickedCheck2)
+	ON_BN_CLICKED(IDCANCEL, &CMAINDLG::OnBnClickedCancel)
 END_MESSAGE_MAP()
 
 
@@ -54,14 +58,10 @@ void CMAINDLG::OnBnClickedButton1()
 
 }
 
-
+//Console调用
 void CMAINDLG::OnBnClickedCheck1()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	
-
-	InitDialog();
-
 
 	//判断是否选中；
 	if (BST_CHECKED == ((CButton*)GetDlgItem(IDC_CHECK1))->GetCheck())
@@ -76,7 +76,8 @@ void CMAINDLG::OnBnClickedCheck1()
 		fprintf(g_Stream, "pDevice:%X\n", (UINT)pDevice);
 		fprintf(g_Stream, "pContext:%X\n", (UINT)pContext);
 		fprintf(g_Stream, "oPresent:%X\n", (UINT)oPresent);
-
+		fprintf(g_Stream, "oDrawIndexed:%X\n", (UINT)oDrawIndexed);
+		
 	}
 		
 	
@@ -87,66 +88,8 @@ void CMAINDLG::OnBnClickedCheck1()
 		FreeConsole();
 
 	}
-
-
-
 }
 
-
-//初始化
-VOID CMAINDLG::InitDialog()
-{
-	
-	if (oPresent == NULL)
-	{
-
-		//交换链
-		DXGI_SWAP_CHAIN_DESC scd;
-		ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));		//填充0
-
-		scd.BufferCount = 1;								//我们只创建一个后缓冲（双缓冲）因此为1
-		scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;  //设置颜色格式,我们使用RGBA
-		scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;  //缩放比
-		scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED; //扫描线
-		scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;  //渲染目标输出
-		scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; //允许模式切换
-		scd.OutputWindow = hWindow;  //在游戏窗体内部绘制另外一个窗口
-		scd.SampleDesc.Count = 1;                      //1重采样
-		scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;      //常用参数
-		scd.Windowed = ((GetWindowLongPtr(hWindow, GWL_STYLE) & WS_POPUP) != 0) ? false : true;  //是否全屏
-		scd.BufferDesc.Width = 1680;
-		scd.BufferDesc.Height = 1050;
-		scd.BufferDesc.RefreshRate.Numerator = 144;     //刷新率
-		scd.BufferDesc.RefreshRate.Denominator = 1;     //分母
-		scd.SampleDesc.Quality = 0;                     //采样等级
-
-		D3D_FEATURE_LEVEL featrueLevel = D3D_FEATURE_LEVEL_11_0;
-
-		D3D11CreateDeviceAndSwapChain(
-			NULL,
-			D3D_DRIVER_TYPE_HARDWARE,
-			NULL,
-			NULL, &featrueLevel,
-			1,
-			D3D11_SDK_VERSION,
-			&scd,
-			&pSwapChain,
-			&pDevice,
-			NULL,
-			&pContext
-		);
-
-		//保存值
-		pSwapChainVT = (DWORD_PTR*)pSwapChain;
-		pDeviceVT = (DWORD_PTR*)pDevice;
-		pContextVT = (DWORD_PTR*)pContext;
-		pSwapChainVT = (DWORD_PTR*)(pSwapChainVT[0]);
-		oPresent = (GD3D11Present)pSwapChainVT [8];
-
-
-	}
-	
-}
 
 HRESULT __stdcall HKD3D11Present(IDXGISwapChain * This, UINT SyncInterval, UINT Flags)
 {
@@ -155,16 +98,18 @@ HRESULT __stdcall HKD3D11Present(IDXGISwapChain * This, UINT SyncInterval, UINT 
 
 	return oPresent(This, SyncInterval, Flags);
 }
+VOID __stdcall HKD3D11DrawIndexed(ID3D11DeviceContext * pContext, UINT IndexCount, UINT StartIndexLocation, INT BaseVertexLocation)
+{
+	fprintf(g_Stream, "22222222222222222222222\n");
 
 
+	oDrawIndexed(pContext, IndexCount, StartIndexLocation, BaseVertexLocation);
+}
 
+//HOOK选项
 void CMAINDLG::OnBnClickedCheck2()
 {
 	// TODO: 在此添加控件通知处理程序代码
-
-
-
-	InitDialog();
 	
 
 		//判断是否选中；
@@ -172,9 +117,11 @@ void CMAINDLG::OnBnClickedCheck2()
 	{
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
-		
+		//HOOK D3D11Present
 		DetourAttach((PVOID*)&oPresent, (PVOID)HKD3D11Present);
-		
+		//HOOK DrawIndexed
+		DetourAttach((PVOID*)&oDrawIndexed, (PVOID)HKD3D11DrawIndexed);
+
 		DetourTransactionCommit();
 	}
 
@@ -186,13 +133,81 @@ void CMAINDLG::OnBnClickedCheck2()
 	{
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
-		
+		//UNHOOK D3D11Present
 		DetourDetach((PVOID*)&oPresent, (PVOID)HKD3D11Present);
-		
+		//UNHOOK DrawIndexed
+		DetourDetach((PVOID*)&oDrawIndexed, (PVOID)HKD3D11DrawIndexed);
+
 		DetourTransactionCommit();
 
 	}
 }
 
 
+//程序关闭
+void CMAINDLG::OnBnClickedCancel()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialog::OnCancel();
+}
 
+//程序初始化
+BOOL CMAINDLG::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	
+	// TODO:  在此添加额外的初始化
+
+	SetDlgItemText(IDC_EDIT1, L"程序开始初始化");
+
+
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));		//填充0
+
+	scd.BufferCount = 1;								//我们只创建一个后缓冲（双缓冲）因此为1
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;  //设置颜色格式,我们使用RGBA
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;  //缩放比
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED; //扫描线
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;  //渲染目标输出
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; //允许模式切换
+	scd.OutputWindow = hWindow;  //在游戏窗体内部绘制另外一个窗口
+	scd.SampleDesc.Count = 1;                      //1重采样
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;      //常用参数
+	scd.Windowed = ((GetWindowLongPtr(hWindow, GWL_STYLE) & WS_POPUP) != 0) ? false : true;  //是否全屏
+	scd.BufferDesc.Width = 1680;
+	scd.BufferDesc.Height = 1050;
+	scd.BufferDesc.RefreshRate.Numerator = 144;     //刷新率
+	scd.BufferDesc.RefreshRate.Denominator = 1;     //分母
+	scd.SampleDesc.Quality = 0;                     //采样等级
+
+	D3D_FEATURE_LEVEL featrueLevel = D3D_FEATURE_LEVEL_11_0;
+
+	sz_Info = D3D11CreateDeviceAndSwapChain(
+		NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL, &featrueLevel,
+		1,
+		D3D11_SDK_VERSION,
+		&scd,
+		&pSwapChain,
+		&pDevice,
+		NULL,
+		&pContext
+	);
+
+	//保存值
+	pSwapChainVT = (DWORD_PTR*)pSwapChain;
+	pDeviceVT = (DWORD_PTR*)pDevice;
+	pContextVT = (DWORD_PTR*)pContext;
+
+	//获取oPresent
+	pSwapChainVT = (DWORD_PTR*)(pSwapChainVT[0]);
+	oPresent = (GD3D11Present)pSwapChainVT[8];
+	//获取DrawIndexed
+	pContextVT = (DWORD_PTR*)(pContextVT[0]);
+	oDrawIndexed = (GD3D11DrawIndexed)(pContextVT[12]);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 异常: OCX 属性页应返回 FALSE
+}
--- MyHackDll/MAINDLG.h
+++ MyHackDll/MAINDLG.h
@@ -5,7 +5,7 @@
 
 //游戏D3D11Present函数定义
 typedef HRESULT(__stdcall *GD3D11Present) (IDXGISwapChain* This, UINT SyncInterval, UINT Flags);
-
+typedef	VOID(__stdcall *GD3D11DrawIndexed) (ID3D11DeviceContext * pContext ,UINT IndexCount, UINT StartIndexLocation, INT BaseVertexLocation );
 
 
 // CMAINDLG 对话框
@@ -25,13 +25,13 @@ public:
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
-
+	virtual BOOL OnInitDialog();
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedButton1();
 	afx_msg void OnBnClickedCheck1();
 	afx_msg void OnBnClickedCheck2();
-	VOID InitDialog();
+	afx_msg void OnBnClickedCancel();
 public:
 
 
@@ -39,17 +39,26 @@ public:
 	//窗口句柄
 	HWND hWindow;
 
+
+	//创建D3D交换链
+	DXGI_SWAP_CHAIN_DESC scd;
+
+
 	//D3D11参数
+	IDXGISwapChain * pSwapChain;
 	ID3D11Device*  pDevice;
 	ID3D11DeviceContext * pContext;
-	IDXGISwapChain * pSwapChain;
 
-	//获取d3d交换链
+
+	//获取d3d交换链值
 	DWORD_PTR* pDeviceVT;
 	DWORD_PTR* pContextVT;
-	DWORD_PTR* pSwapChainVT;
+	DWORD_PTR* pSwapChainVT ;
+
+	//判断是否执行成功
+	HRESULT sz_Info;
 
 	
 
-	
+
 };
